ddfe55d0b3e9b4445c83584090c6385edc8bb812
eL <68808205+love-droid@users.noreply.github.com>
2023-10-25 10:46:00 +0530

    app setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='border-solid border-2 border-indigo-600 m-10 p-5 text-center'>React App</h1>
+      <div className="
+h-full w-full bg-green-500 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border border-gray-100
+"></div>
     </div>
   );
 }
